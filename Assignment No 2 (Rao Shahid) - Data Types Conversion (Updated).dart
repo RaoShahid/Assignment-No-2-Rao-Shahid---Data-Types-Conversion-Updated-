dfc2224ad6356afc1266bfe7a0b999df843e6f62
RaoShahid <rao.shahid.1033@gmail.com>
2022-03-11 22:07:18 +0500

    Add files via upload

---
+++ Assignment No 2 (Rao Shahid) - Data Types Conversion (Updated).dart
@@ -0,0 +1,42 @@
+main ()
+{
+                       // Assignment No:2 - Data Types Conversion
+
+///////////////////////Convert String into Integer and Double Data Type//////////////////////////////
+String salary= '30000';
+
+int salaryInInteger= int.parse(salary);
+print("Salary in String to Integer: $salaryInInteger");
+//Salary in String to Integer: 30000
+
+double salaryInDouble= double.parse(salary);
+print("Salary in String to Double : $salaryInDouble");
+//Salary in String to Double : 30000.0
+
+/////////////////////Covert Double and Integer into String Data Type///////////////////////////////////
+int firstStudentFee= 6315;
+int secoundStudentFee= 4112;
+
+int sumOfStudentsFee=firstStudentFee+secoundStudentFee;
+String sumOfStudentFeeInString= sumOfStudentsFee.toString();
+print("Sum of Student Fees Integer to String: $sumOfStudentFeeInString");
+//Sum of Student Fees Integer to String: 10427
+
+double avgFee= (firstStudentFee+secoundStudentFee)/2;
+String avgFeeInString=avgFee.toString();
+print("Average Student Fees Double to String: $avgFeeInString");
+//Average Student Fees Double to String: 5213.5
+
+//////////////////////////////////Convert Double to Integer///////////////////////////////////////////
+int marks=20;
+double marksInDouble=marks.toDouble();
+print("Marks in Integer to Double: $marksInDouble");
+//Marks in Integer to Double: 20.0
+
+//////////////////////////////////Convert Double to Integer///////////////////////////////////////////
+double result=15.5;
+int resultInInteger=result.toInt();
+print("Marks in Double to Intager: $resultInInteger");
+//Marks in Double to Intager: 15
+
+}
